91fccd5fd0a83edd4a70c460462ce91e675c3da7
thato61 <thato61@U-2GPIH25HOCM4E.corp.amazonworkspaces.com>
2026-03-13 23:10:14 +0200

    Allowed hosts update

--- DataSenseHub/settings.py
+++ DataSenseHub/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--7r-t&ojq$z62^^w^%zxc1*8^i1ch+6_uum@aatv=1sdyw48on
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ec2-44-222-123-255.compute-1.amazonaws.com", "https://44.222.123.255","localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["ec2-44-222-123-255.compute-1.amazonaws.com", "https://44.222.123.255","localhost", "127.0.0.1", "http://52.90.3.181:8000"]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://ec2-44-222-123-255.compute-1.amazonaws.com',
